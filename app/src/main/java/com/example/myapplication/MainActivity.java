5910836e43df4c31344f4acbdf76c933ca0f4895
maryembahloul <maryembahlool1996@gmail.com>
2019-11-24 21:36:19 +0100

    TP2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        Log.d("Question1:", "Réponse:La deuxième activité est ajoutée en tant que classe Java, le fichier de présentation (layout) XML " +
+                "est créé et le fichier AndroidManifest.xml est modifié pour déclarer une deuxième activité.");
+        Log.d("Question2:", "Réponse: Le bouton Back (Précédent) ne fonctionne plus dans la deuxième activité pour " +
+                "renvoyer l'utilisateur à l'activité principale.");
+        Log.d("Question3:", "Réponse: new Intent(Context context, Class<?> class)");
+        Log.d("Question4:", "Réponse: Comme extra d'intention (Intent)");
+        Log.d("Question5:", "Tout ce qui précède");
+
+
     }
 
     public void countUp(View view) {
